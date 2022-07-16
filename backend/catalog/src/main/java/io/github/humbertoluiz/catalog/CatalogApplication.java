6beb5ed35dac869c03d74e7d222427ecb2315f06
HumbertoLuiz <kaos_132@hotmail.com>
2022-07-16 17:48:20 -0300

    Starting with categories

---
+++ backend/catalog/src/main/java/io/github/humbertoluiz/catalog/CatalogApplication.java
@@ -0,0 +1,13 @@
+package io.github.humbertoluiz.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogApplication.class, args);
+	}
+
+}
