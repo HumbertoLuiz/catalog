24473ecb8715f93f23ab6fda30620f88041e97a3
HumbertoLuiz <kaos_132@hotmail.com>
2022-07-16 20:43:09 -0300

    DTO

--- backend/catalog/src/main/java/io/github/humbertoluiz/catalog/resources/CategoryResource.java
+++ backend/catalog/src/main/java/io/github/humbertoluiz/catalog/resources/CategoryResource.java
@@ -1,12 +1,13 @@
 package io.github.humbertoluiz.catalog.resources;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import io.github.humbertoluiz.catalog.entities.Category;
+import io.github.humbertoluiz.catalog.dto.CategoryDTO;
 import io.github.humbertoluiz.catalog.services.CategoryService;
 
 @RestController
@@ -17,8 +18,8 @@ public class CategoryResource {
 	private CategoryService categoryService;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = categoryService.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = categoryService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
