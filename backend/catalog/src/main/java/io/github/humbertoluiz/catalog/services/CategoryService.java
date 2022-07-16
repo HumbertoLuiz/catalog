feb5d93b7e0dae78fb307116b0a5603e3e25f823
HumbertoLuiz <kaos_132@hotmail.com>
2022-07-16 20:22:23 -0300

    Database Seeding

--- backend/catalog/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
+++ backend/catalog/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.github.humbertoluiz.catalog.entities.Category;
 import io.github.humbertoluiz.catalog.repositories.CategoryRepository;
@@ -14,7 +15,10 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Transactional( readOnly = true )
 	public List<Category> findAll() {
 		return categoryRepository.findAll();
 	}
+	
+	
 }
---
+++ backend/catalog/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into tb_category(name) values('Books');
+insert into tb_category(name) values('Eletronics');
+insert into tb_category(name) values('Computers');
