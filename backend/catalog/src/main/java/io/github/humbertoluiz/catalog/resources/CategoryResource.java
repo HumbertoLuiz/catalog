4a44765a3cc3bfb0c4fb8606f2ad6415d41d4fcf
HumbertoLuiz <kaos_132@hotmail.com>
2022-07-17 19:03:41 -0300

    UPDATE new Category

--- backend/catalog/src/main/java/io/github/humbertoluiz/catalog/resources/CategoryResource.java
+++ backend/catalog/src/main/java/io/github/humbertoluiz/catalog/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,9 +41,12 @@ public class CategoryResource {
 		catDTO = categoryService.insert(catDTO);
 		URI uri =	ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(catDTO.getId()).toUri();
-		return ResponseEntity.created(uri).body(catDTO);
-	
+		return ResponseEntity.created(uri).body(catDTO);	
 	}
 	
-	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update( @RequestBody CategoryDTO catDTO, @PathVariable Long id ) {
+		catDTO = categoryService.update(catDTO, id);
+		return ResponseEntity.ok().body(catDTO);	
+	}
 }
--- backend/catalog/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
+++ backend/catalog/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import io.github.humbertoluiz.catalog.dto.CategoryDTO;
 import io.github.humbertoluiz.catalog.entities.Category;
 import io.github.humbertoluiz.catalog.repositories.CategoryRepository;
-import io.github.humbertoluiz.catalog.services.exception.EntityNotFoundException;
+import io.github.humbertoluiz.catalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -30,7 +32,7 @@ public class CategoryService {
 	@Transactional( readOnly = true )
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = categoryRepository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 
@@ -40,5 +42,18 @@ public class CategoryService {
 		entity.setName(catDTO.getName());
 		entity = categoryRepository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(CategoryDTO catDTO, Long id) {
+		try {
+			Category entity = categoryRepository.getReferenceById(id);
+			entity.setName(catDTO.getName());
+			entity = categoryRepository.save(entity);
+			return new CategoryDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
 	}		
 }
