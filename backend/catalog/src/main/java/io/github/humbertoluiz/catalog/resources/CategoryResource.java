d119ce164337f611979682f0534ff53f96c04769
HumbertoLuiz <kaos_132@hotmail.com>
2022-07-17 19:21:20 -0300

    DELETE Category

--- backend/catalog/src/main/java/io/github/humbertoluiz/catalog/resources/CategoryResource.java
+++ backend/catalog/src/main/java/io/github/humbertoluiz/catalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,11 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> update( @RequestBody CategoryDTO catDTO, @PathVariable Long id ) {
 		catDTO = categoryService.update(catDTO, id);
 		return ResponseEntity.ok().body(catDTO);	
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update( @PathVariable Long id ) {
+		categoryService.delete(id);
+		return ResponseEntity.noContent().build();	
 	}
 }
--- backend/catalog/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
+++ backend/catalog/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import io.github.humbertoluiz.catalog.dto.CategoryDTO;
 import io.github.humbertoluiz.catalog.entities.Category;
 import io.github.humbertoluiz.catalog.repositories.CategoryRepository;
+import io.github.humbertoluiz.catalog.services.exceptions.DatabaseException;
 import io.github.humbertoluiz.catalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -54,6 +57,18 @@ public class CategoryService {
 		}
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+		categoryRepository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
 		}
 	}		
 }
