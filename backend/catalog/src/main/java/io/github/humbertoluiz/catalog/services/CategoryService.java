0d2b98133a655b1b6b4b1eae43e765458dc6f58f
HumbertoLuiz <kaos_132@hotmail.com>
2022-07-17 13:19:44 -0300

    Find Category by id, Exception Handling

--- backend/catalog/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
+++ backend/catalog/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package io.github.humbertoluiz.catalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import io.github.humbertoluiz.catalog.dto.CategoryDTO;
 import io.github.humbertoluiz.catalog.entities.Category;
 import io.github.humbertoluiz.catalog.repositories.CategoryRepository;
+import io.github.humbertoluiz.catalog.services.exception.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -23,6 +25,13 @@ public class CategoryService {
 		return list.stream()
 				.map(cat -> new CategoryDTO(cat))
 				.collect(Collectors.toList());
+	}
+	
+	@Transactional( readOnly = true )
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 		
 }
