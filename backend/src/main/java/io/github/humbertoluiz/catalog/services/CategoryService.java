4c45f3a9d2d58663b906a6281ef1baf809de5761
HumbertoLuiz <kaos_132@hotmail.com>
2022-07-22 19:49:51 -0300

    Refactor Pageable

--- backend/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
+++ backend/src/main/java/io/github/humbertoluiz/catalog/services/CategoryService.java
@@ -1,14 +1,17 @@
 package io.github.humbertoluiz.catalog.services;
 
 import java.util.Optional;
+
 import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import io.github.humbertoluiz.catalog.dto.CategoryDTO;
 import io.github.humbertoluiz.catalog.entities.Category;
 import io.github.humbertoluiz.catalog.repositories.CategoryRepository;
@@ -22,8 +25,8 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional( readOnly = true )
-	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Category> list = categoryRepository.findAll(pageRequest);
+	public Page<CategoryDTO> findAllPaged(Pageable pageable) {
+		Page<Category> list = categoryRepository.findAll(pageable);
 		return list.map(cat -> new CategoryDTO(cat));
 	}
 	
--- backend/src/main/java/io/github/humbertoluiz/catalog/services/ProductService.java
+++ backend/src/main/java/io/github/humbertoluiz/catalog/services/ProductService.java
@@ -1,14 +1,17 @@
 package io.github.humbertoluiz.catalog.services;
 
 import java.util.Optional;
+
 import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import io.github.humbertoluiz.catalog.dto.CategoryDTO;
 import io.github.humbertoluiz.catalog.dto.ProductDTO;
 import io.github.humbertoluiz.catalog.entities.Category;
@@ -28,8 +31,8 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional( readOnly = true )
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Product> list = productRepository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Pageable pageable) {
+		Page<Product> list = productRepository.findAll(pageable);
 		return list.map(cat -> new ProductDTO(cat));
 	}
 	
